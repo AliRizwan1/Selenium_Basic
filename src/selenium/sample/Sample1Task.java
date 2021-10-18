3bad4a42da24f5cbc647e49ee00e5ae763094c5b
Ali Rizwan <alirizwanbutt1994@gmail.com>
2021-10-18 13:52:51 +0300

    activity 1

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -13,10 +13,18 @@ public class Sample1Task {
     @Test
     public void goToHomepage() throws Exception {
 //        TODO:
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver" + new selenium.ChangeToFileExtension().extension());
 //         define driver
+        WebDriver driver = new ChromeDriver();
+
 //         go to https://kristinek.github.io/site/index2.html
+        //driver.get("https://kristinek.github.io/site/index2.html");
 //         get title of page
+        System.out.println(driver.getTitle());
 //         get URL of current page
+        System.out.println(driver.getCurrentUrl());
 //         close browser
+        Thread.sleep(10000);
+        driver.quit();
     }
 }
